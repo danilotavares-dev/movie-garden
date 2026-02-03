06f0b4cd9838069bd1d42fe54d4a2b29c3ba49ac
danilotavares-dev <danilotavarespr00@gmail.com>
2026-02-03 00:45:44 -0300

    feat: adiciona um novo gradient.

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -10,6 +10,9 @@ export default {
       backgroundImage: {
         'custom-gradient':
           'linear-gradient(135deg, rgba(255,255,255,1) 0%, rgba(188,166,166,1) 26%, rgba(111,167,139,1) 60%, rgba(80,136,108,1) 78%, rgba(1,43,22,1) 100%)',
+
+        'custom-gradient-night':
+          'linear-gradient(166deg, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 47%, rgba(0,6,13,1) 89%, rgba(0,28,13,1) 99%)',
       },
     },
   },
