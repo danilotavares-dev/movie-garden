e6ba866f0968aa242376efecc84d3a3c11a28ccc
danilotavares-dev <danilotavarespr00@gmail.com>
2026-02-03 22:34:09 -0300

    feat: adiciona uma nova rota para a autenticação para o servidor do python.

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,14 +1,21 @@
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import { z } from 'zod'
 import { PrismaClient } from '@prisma/client'
 import { hash, compare } from 'bcryptjs'
+import { getRecommendations } from './controllers/recommendation.controller'
+import { verifyJwt } from './middlewares/verify-jwt'
 
 const app = fastify()
 const prisma = new PrismaClient()
 
 app.register(cors, {
   origin: '*',
+})
+
+app.register(jwt, {
+  secret: 'movie-garden-secret-key-123',
 })
 
 app.post('/users', async (request, reply) => {
@@ -21,10 +28,9 @@ app.post('/users', async (request, reply) => {
   const { name, email, password } = registerBodySchema.parse(request.body)
 
   const userAlreadyExists = await prisma.user.findUnique({
-    where: {
-      email,
-    },
+    where: { email },
   })
+
   if (userAlreadyExists) {
     return reply.status(409).send({ message: 'E-mail already exists.' })
   }
@@ -53,11 +59,7 @@ app.post('/login', async (request, reply) => {
 
   const { email, password } = loginBodySchema.parse(request.body)
 
-  const user = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  })
+  const user = await prisma.user.findUnique({ where: { email } })
 
   if (!user) {
     return reply.status(400).send({ message: 'E-mail ou senha inválido(s).' })
@@ -69,11 +71,23 @@ app.post('/login', async (request, reply) => {
     return reply.status(400).send({ message: 'E-mail ou senha inválido(s).' })
   }
 
+  const token = await reply.jwtSign(
+    {},
+    {
+      sign: {
+        sub: user.id,
+      },
+    },
+  )
+
   return reply.status(200).send({
     userId: user.id,
+    token,
     message: 'Login realizado com sucesso!.',
   })
 })
+
+app.post('/recommendations', getRecommendations)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP Server runnning on http://localhost:3333')
